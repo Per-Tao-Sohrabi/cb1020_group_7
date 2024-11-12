0136b06e7bc50c656957de6fb996f6c56b584ce4
Yohans03 <yshamil030411@gmail.com>
2024-11-12 18:26:46 +0100

    Added some bacis things for the macrophage class in testing purposes

--- Macrophage_class.py
+++ Macrophage_class.py
@@ -1,1 +1,4 @@
 # Här tänker jag att klassen för makrofag M1 & M2 ska vara.
+
+class Macrophage: # This class contains every action and interaction that the macrophages will have
+    def __init__(self, ): # The attributen from...
