6060b25673faf44f9145b03f828624b4cfd8f4cf
per8868.test <per.sohrabi@yahoo.com>
2024-11-28 13:47:03 +0100

    added lists for each object in MianModel.py

---
+++ src/MainModel.py
@@ -0,0 +1,12 @@
+import mesa as ms
+
+class MainModel:
+
+    def __init__(self, macrophage, fibroblast, cells):
+        #Fields
+        
+        #lista för macrophages
+        macList = [];
+        fibList = [];
+        cells = [];
+
