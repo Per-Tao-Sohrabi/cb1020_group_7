0d09ebce22943d07a00ec2662959838575c5805d
per8868.test <per.sohrabi@yahoo.com>
2024-11-28 20:14:25 +0100

    Minor updates to Cells.py

--- src/Cells.py
+++ src/Cells.py
@@ -7,4 +7,11 @@ class Cells(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.unique_id = unique_id;
-        model = model; 
+
+        #location
+
+        #cancerous state
+
+        #
+
+    #Growth method
