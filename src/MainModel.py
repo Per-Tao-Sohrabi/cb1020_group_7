1c1f8ea9a1420c20312812de6d8b6797ce95b7e7
per8868.test <per.sohrabi@yahoo.com>
2024-11-28 14:54:04 +0100

    Cells.py added

--- src/MainModel.py
+++ src/MainModel.py
@@ -3,14 +3,23 @@ from mesa.space import SingleGrid
 from mesa.time import RandomActivation
 
 class MainModel(Model):
+    #inheret methods from mesa Model class;J
+    super.__init__; 
 
     def __init__(self, macrophage, fibroblast, cells):
+       
         #Fields
         
         #lista för macrophages
         self.macList = [];
         self.fibList = [];
         self.cells = [];
+        '''Other inhereted fields:
+            running
+            steps
+            random
+            rng
+        '''
 
         #initiate game environment
 
