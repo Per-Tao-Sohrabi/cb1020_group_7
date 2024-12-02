5ca6e60da19ba77e26954578052655a84b3a57fd
per8868.test <per.sohrabi@yahoo.com>
2024-12-03 00:17:01 +0100

    Cleaned up filenames, created new files, added mesa import to all agent files

--- src/Cells.py
+++ src/Cells.py
@@ -1,11 +1,12 @@
 from mesa import Agent;
 
 class Cells(Agent):
-    super().__init__;
+    
 
     #Constructor
     def __init__(self, unique_id, model):
-        super().__init__(unique_id, model)
+        super().__init__(model);
+
         self.unique_id = unique_id;
 
         #location
