089b09c3a9ecd639962d1dad78cb47300b77a679
Yohans03 <yshamil030411@gmail.com>
2024-12-02 01:21:16 +0100

    I have fixed the main-class that operates the whole simulation so that it is better optimized for our project, dvs. this is the first draft for the main class.

--- src/Main_test_grid.py
+++ src/Main_test_grid.py
@@ -1,64 +1,109 @@
+# Imports all the required classes from the needed modules for creating the Mesa-model
+import numpy as np; import random 
+from mesa import Agent, Model 
+from mesa.time import RandomActivation; from mesa.space import MultiGrid; from mesa.datacollection import DataCollector
+
+
+
+class TU_Cell_Agent(Agent):
+    def __init__(self, agent_id, model, cell_type):
+        super.__init__(agent_id, model)                 # "Super" inherit the parent class's (Agent) instance variables
+        self.cell_type = cell_type                      # Add the type of cell as an additional instance variable
+
+
+class Tumor_cell(TU_Cell_Agent):
+    def __init__(self, agent_id, model, "TU_cell")
+        self.cancerous = True                           # All the relevant properties (instance variables) for the tumor cell are initiated 
+        self.viable = True
+        self.proliferation_prob = 0.0846
+        self.migration_prob = 0.1167
+        self.death_prob = 0.00284
+        self.resistance_Macrophage_prob = 0.004
+        # OBS: Lägg till flera relevanta instansvariabler!!
+    
+    def apoptosis(): # Försöka modellera om cellen är tillräckligt nära en anti-cancer-makrofag så dödas den mha. denna metod
+        if agent_id == abs(): 
+            self.viable = False
+        else: 
+            None
+
+    # Exempel på resterande metoder som tumörcellen kommer att behöva 
+    def step():
+
+    def migration():
+
+    def proliferation():
+    
+    def resistance_Macrohpage():
+
+
+
+
+# OBS: Varje klass som hör till en unik cell-typ bör vara i en separat fil sen (nu är dem här för att kunna testköra koden)
+class Macrophage_M1(TU_Cell_Agent): 
+    def __init__(self, agent_id, model, "Macrophage_M1"):
+        self.anti_cancerous = False
+        self.proliferation_prob = 0.0065
+        # etc. lägg till flera instansvariabler
+        
+class Fibroblast(TU_Cell_Agent):
+    def __init__(self, agent_id, model, Fibroblast"):
+        # Samma här, addera instansvariabler
+
+class Endothelial(TU_Cell_Agent):
+    def __init__(self, agent_id, model, "Endothelial"):
+        self.proliferation_prob = 0.02
 
 
 
 
 
+# The main class definies the key-parameters for the run of the ABM prostate cancer simulation
 class Main_simulation(Model):
     def __init__(self, width, height):
-        self.grid = Grid(width, height, torus=False) # torus = False 
-        self.schedule = RandomActivation(self)       # activates each agent randomly one time per step in the simulation
-        self.agent_ID = 0                            # Creates unique identification "agent_ID" for each agent in every iteration
-        self.run_ABM = True
+        super().__init__()
+        self.grid = MultiGrid(width, height, True)   # Creates a 2D-grid
+        self.schedule_cell = RandomActivation(self)  # Activates each agent randomly one time per step in the simulation
+        self.agent_id = 0                            # Creates unique identification "agent_ID" for each agent in every iteration
+        self.run_ABM = True                          # Runs the AMB-simulation
 
         # Place the initial tumour cell in the grid
-        initial_tumor = TumorCell(self.next_id(), self)
-        x = self.random.randrange(self.grid.width)
-        y = self.random.randrange(self.grid.height)
-        self.grid.place_agent(initial_tumor, (x, y))
-        self.schedule.add(initial_tumor)
+        initial_tumor = Tumor_Cell(self.agent_id, self)   # Creates a new instance of the class Tumor_Cell 
+        x = self.random.randrange(self.grid.width)        # Creates a random x-coordinate within the grid's width
+        y = self.random.randrange(self.grid.height)       # Creates a random y-coordinate within the grid's width
+        self.grid.place_agent(initial_tumor, (x, y))      # Places the agent in the correct place
+        self.schedule_cell.add(initial_tumor)
+        
 
-        # Datainsamling
+        # Collects the data for how many tumor cells we have
         self.datacollector = DataCollector(
-            model_reporters={"TumorCellCount": lambda m: sum(1 for agent in m.schedule.agents if isinstance(agent, TumorCell))}
+            model_reporters={"Number_tumor_Cells": lambda m: sum(1 for agent in m.schedule_cell.agents if isinstance(agent, Tumor_Cell))}
         )
 
-    def step(self):
-        self.datacollector.collect(self)
-        self.schedule.step()
+    def step(self):                                       # This method takes steps in the simulation
+        self.datacollector.collect(self)                  # It collects all the required data as well
+        self.schedule_cell.step()                         
 
-    def next_id(self):
-        self.agent_ID += 1
-        return self.agent_ID
+    def next_agent_id(self):                              # Keeps track of the "agent_id", crucial for knowing the composition of the cells in the end
+        self.agent_id += 1
+        return self.agent_id
 
 
-def run_AMBsim(width=125, height=125, steps=1000): # This function runs the whole simulation 
-    simulation_run = Main_simulation(width, height)
+# This function runs the whole simulation 
+def run_AMB_sim(width=125, height=125, steps=1000): 
+    model = Main_simulation(width, height)
     for _ in range(steps):
-        simulation_run.step()
+        model.step()
     return model.datacollector.get_model_vars_dataframe()
 
 if __name__ == "__main__":
-    results = run_ABMsim()
+    results = run_ABM_sim()
     print(results)
 
+# The main class code is finished here!!
 
 
 
 
 
 
-
-
-
-# Skräpkod 
-import mesa as ms
-class MainModel:
-
-    def __init__(self, macrophage, fibroblast, cells):
-        #Fields
-        
-        #lista för macrophages
-        macList = [];
-        fibList = [];
-        cells = [];
-        endothelial = [];
