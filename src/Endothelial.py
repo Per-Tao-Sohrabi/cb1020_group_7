0ca671dd5b0ae1c7c9ce73d1a3d539593798d3a6
yshamil030411@gmail.com <yshamil030411@gmail.com>
2024-12-22 12:12:51 +0100

    ...

--- src/Endothelial.py
+++ src/Endothelial.py
@@ -8,13 +8,13 @@ from mesa import Agent;
 class Endothelial(Agent):
     #STATIC FIELDS
     def get_class_name(self):
-         return "Endothelial"
+        return "Endothelial"
 
     def __init__(self, unique_id, position, model): #Position inputed as (x,y)
         super().__init__(unique_id, model)
         self.unique_id
         self.model = model
-        self.position = position;
+        self.position = position
     
     def step(self):
         #define behaviour for diff situations here
