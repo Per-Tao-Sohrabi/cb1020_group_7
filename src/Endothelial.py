2d4d9ad26ca3f0839b1da2c65953a9ae08f14335
yshamil030411@gmail.com <yshamil030411@gmail.com>
2024-12-22 23:37:30 +0100

    icke fungerande kod för Endothelial cells, under utveckling

--- src/Endothelial.py
+++ src/Endothelial.py
@@ -1,4 +1,5 @@
 from mesa import Agent;
+import random as random;
 '''
 # Endothelial class
 ### Desricption:
@@ -17,5 +18,26 @@ class Endothelial(Agent):
         self.position = position
     
     def step(self):
-        #define behaviour for diff situations here
+        if position > abs()
+        # Define behaviour for diff situations here
         pass
+
+    def proliferate(self):
+        empty_cells = [cell for cell in self.model.grid.get_neighborhood(self.pos, moore=True, include_center=False) 
+                       if self.model.grid.is_cell_empty(cell)]
+        if empty_cells:
+            if endo_type == "horizontal cell":
+                x_inc = random.randint(0,1)
+                y_inc = random.randint(-1,1)
+                x_coord = 0
+                y_coord = random.randrange(0, 1)
+                new_position = self.choice(empty_cells)
+                new_agent = Endothelial(self.model.next_id(), self.model)
+                self.model.grid.place_agent(new_agent, new_position)
+                self.model.schedule.add(new_agent)
+            
+            elif endo_type == "vertical cell":
+                
+                new_agent = Endothelial(self.model.next_id(), self.model)
+                self.model.grid.place_agent(new_agent, new_position)
+                self.model.schedule.add(new_agent)
